c39288960eaae47c1728c59862d54b1831c95263
zhengzl0621 <167868341+ZhulinZheng@users.noreply.github.com>
2025-12-11 09:10:56 +0800

    Delete benchmark/ccps directory

--- benchmark/ccps/run.sh
+++
@@ -1,4 +0,0 @@
-#gdb --args ./build/test/run_tests --gtest_filter=UCITopTest.LoopbackTest
-./build/test/run_tests --gtest_filter=CCPSTopTest.LoopbackTest
-#./build/test/run_tests
-#./build/test/run_tests --gtest_filter=AsyncQueueTests
